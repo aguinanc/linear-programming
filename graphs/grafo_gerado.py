8abb6025dfb497b73ae7b52855438b26a91117e0
Andrei Pereira <andrei.pereira@lnls.br>
2020-08-02 20:22:22 -0300

    Improve examples

--- graphs/grafo_gerado.py
+++ graphs/grafo_gerado.py
@@ -2,52 +2,63 @@
 
 #################################################
 # Comparação dos Algoritmos de Prim e Kruskal
-# para a NSFNET
+# para a grafo gerado automaticamente
 
 ########################### Executar com Python 3 ###########################
 
-from numpy import array
 from datetime import datetime
 from datetime import timedelta
 import sys
+from random import randint
+from random import choice
 import util
 import agm
 
 #######################################################################
 # Apresentacao para usuario
 print('\n\n')
-print('                Arvore Geradora Minima - Grafo NSFNET\n')
+print('                Arvore Geradora Minima - Grafo Gerado\n')
 print('                Aplicacao Metodos Prim e Kruskal\n')
 print('                ----------------------------\n\n\n')
 
-# Definicao de tabela representando NSFNET
+############################
+# Variaveis
 
-matriz_nsfnet = array(\
-[ \
-[ 1,  2,   3,   4,   5,  6,   7,   8,  9,  10, 11, 12,  13,  14, 15, 16,  17, 18,  19, 20,  21,  22, 23, 24,  25], \
-[ 1,  1,   2,   2,   2,  3,   3,   4,  5,   5,  6,  6,   7,   7,  8,  9,  10, 11,  11, 12,  12,  13, 13, 14,  15], \
-[ 2,  5,   3,   4,   9,  4,   5,   7,  6,  12,  7,  8,  10,  14,  9, 11,  11, 13,  15, 13,  15,  14, 16, 15,  16], \
-[75, 120, 75, 120, 300, 75, 120, 150, 60, 300, 75, 60, 150, 300, 60, 60, 105, 75, 120, 60, 120, 120, 75, 60, 120] \
-] )
+num_v = 200     # num de vertices
+num_a = 1000    # num de arestas
+max_custo = 100 # maximo custo de uma aresta
+min_custo = 1   # minimo custo de uma aresta
 
-# Cria grafo NSFNET baseado em matriz do problema
-nsfnet = agm.Grafo('NSFNET')
+## Gera grafo
+grafo_gerado = agm.Grafo('Grafo Gerado')
 
-for col in range(0, matriz_nsfnet.shape[1]):
-    # criar arco (orig, dest, custo, nome)
-    nsfnet.cria_arco(matriz_nsfnet[:,col][1], matriz_nsfnet[:,col][2], matriz_nsfnet[:,col][3], matriz_nsfnet[:,col][0]) 
-    # criar arco (dest, orig, custo, nome)
-    nsfnet.cria_arco(matriz_nsfnet[:,col][2], matriz_nsfnet[:,col][1], matriz_nsfnet[:,col][3], matriz_nsfnet[:,col][0]) 
+# gera arvore principal (v-1 arestas)
+for v in range(1, num_v):
+    # cria aresta vn -> vn+1, custo_n, nome=n
+    grafo_gerado.cria_aresta(v, v+1, randint(min_custo, max_custo), v)
+
+# gera conexoes restantes
+for a in range(num_v, num_a+1):
+    # seleciona vertice 1 aleatoriamente
+    v1 = randint(1, num_v)
+    # obtem vizinhos ja conectados a v1
+    lista_indisponiveis = grafo_gerado.vizinhos(v1)
+    # acrescenta v1 aos vertices indisponiveis
+    lista_indisponiveis.append(v1)
+    # seleciona vertice 2 aleatoriamente (v2 != indisponiveis)
+    v2 = choice([i for i in range(1, num_v) if i != lista_indisponiveis])
+    # cria aresta v1 -> v2, custo, nome=a
+    grafo_gerado.cria_aresta(v1, v2, randint(min_custo, max_custo), a)
 
 # Aplica algoritmo de Prim e registra tempo
 t1 = datetime.now()
-res_prim = agm.prim(nsfnet)
+res_prim = agm.prim(grafo_gerado)
 t2 = datetime.now()
 deltat_prim = t2 - t1
 
 # Aplica algoritmo de Kruskal e registra tempo
 t1 = datetime.now()
-res_kruskal = agm.kruskal(nsfnet)
+res_kruskal = agm.kruskal(grafo_gerado)
 t2 = datetime.now()
 deltat_kruskal = t2 - t1
 
@@ -63,20 +74,20 @@ print('\n')
 
 # cria estrutura de Grafo com resultado
 # para posteriormente salvar em excel
-g_res_prim = agm.Grafo('AGM_Prim_NSFNET')
+g_res_prim = agm.Grafo('AGM_Prim_Grafo_Gerado')
 for item in res_prim:
     g_res_prim.cria_aresta(item[0], item[1], item[2], item[3])
 
-g_res_kruskal = agm.Grafo('AGM_Kruskal_NSFNET')
+g_res_kruskal = agm.Grafo('AGM_Kruskal_Grafo_Gerado')
 for item in res_kruskal:
     g_res_kruskal.cria_aresta(item[0], item[1], item[2], item[3])
 
 # salva grafos em arquivos excel
 print('Salvando matrizes de adjacencia... ')
-print(nsfnet.nome()+'.xlsx')
+print(grafo_gerado.nome()+'.xlsx')
 print(g_res_prim.nome()+'.xlsx')
 print(g_res_kruskal.nome()+'.xlsx')
-util.salva_grafo_excel(nsfnet, nsfnet.nome())
+util.salva_grafo_excel(grafo_gerado, grafo_gerado.nome())
 util.salva_grafo_excel(g_res_prim, g_res_prim.nome())
 util.salva_grafo_excel(g_res_kruskal, g_res_kruskal.nome())
 print('\n')
